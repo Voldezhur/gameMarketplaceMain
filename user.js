a7c851d61c5d8fb0fc78160efb8fe8fcbfba2e0c
Voldezhur <voldezhur@gmail.com>
2024-03-02 14:00:34 +0300

    added test html file

--- user.js
+++ user.js
@@ -1,4 +1,5 @@
 exports.userinfo = (req, res) => {
-    res.send({err:'', user: {name:'', uid: 1}})
+    // res.send({err:'', user: {name:'', uid: 1}}) 
+    res.sendFile('./test.html', {root: __dirname})
 }
 
