8fcc394de6788ddceaa6030965740fcf127129ff
Voldezhur <voldezhur@gmail.com>
2024-03-02 13:55:29 +0300

    fixed routing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const routers = require('./routers')
 
-app.use('/api', routers.router)
+app.use('/', routers.router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
