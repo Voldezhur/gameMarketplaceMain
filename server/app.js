c76597fcf513f3c8acab01b19d897af98a392cd1
Max <max200704@gmail.com>
2024-03-21 09:05:41 +0300

    add token validate and user login

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
 
 const port = 8000;
-const router = require('./router');
 
 const cors = require('cors');  // чтобы не было ошибки cors
 const corsOption = {
@@ -10,17 +12,31 @@ const corsOption = {
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
-
 app.use(cors(corsOption))
+app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
+app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.use(cors())
 
-app.use('/', router.mainRouter);
+// add to req bearer token (if exist)
+app.use((req, _res, next) => {
+  const bearer = req.headers['authorization'];
+  if (bearer?.length) {
+    req.token = bearer.split(' ')?.[1] || null;
+  }
+  next();
+})
+
+//add to req postgres pool
+app.use((req, res, next) => {
+  req.db = require("./postgresConfig")
+  next()
+})
+
+app.use('/api', require('./router').mainRouter);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`)
--- server/router.js
+++ server/router.js
@@ -1,11 +1,19 @@
-const RouterClass = require('express').Router
+const RouterClass = require('express').Router;
 
-const mainRoutes = require('./oldRoutes.js')
-const queries = require("./databaseQueries.js")
+// const mainRoutes = require('./oldRoutes.js')
+const { checkAuth } = require('./users');
+const Catalog = require("./catalog.js");
+const Users = require("./users.js");
+const mainRouter = new RouterClass();
 
-const mainRouter = new RouterClass()
-mainRouter.get('/usersList', queries.getAllUsers)
-mainRouter.get('/gamesList', queries.getAllGames)
-mainRouter.get('/user/:id', queries.getUserById)
+// Users
+mainRouter.get('/user/list', Users.getAllUsers);
+mainRouter.post('/user/login', Users.login);
+mainRouter.get('/user/logout', Users.logout);
+mainRouter.get('/user/info', checkAuth, Users.getUserById);
+
+// Catalog
+mainRouter.get('/games-list', Catalog.getAllGames);
+
 
 module.exports.mainRouter = mainRouter
